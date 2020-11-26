30011b8567ba6cec9f0c29967d1ac775a887e7be
Trevyn <trevyncase@gmail.com>
2020-11-26 00:22:57 -0600

    Now filtering main page to only show messages of users user is following

--- app.py
+++ app.py
@@ -317,6 +317,7 @@ def homepage():
     if g.user:
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_([user.id for user in g.user.following]))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
